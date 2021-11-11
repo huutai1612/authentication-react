8e01c008147163c3995c736fda4bd107d3bb3f55
huutai1612 <tai.huu1612@gmail.com>
2021-11-11 13:10:18 +0700

    done adding change password feture

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,73 @@
+import { useContext, useRef } from 'react';
+import { useHistory } from 'react-router-dom';
+import useHttp from '../../hooks/use-http';
+import AuthContext from '../../store/auth-context';
 import classes from './ProfileForm.module.css';
 
+const isValid = (value) => value.trim().length >= 6;
+
 const ProfileForm = () => {
-  return (
-    <form className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
-      </div>
-      <div className={classes.action}>
-        <button>Change Password</button>
-      </div>
-    </form>
-  );
-}
+	const sendRequest = useHttp;
+	const authContext = useContext(AuthContext);
+	const passwordInputRef = useRef();
+	const history = useHistory();
+
+	const idToken = authContext.token;
+
+	const submitHandler = (event) => {
+		event.preventDefault();
+		const enteredPassword = passwordInputRef.current.value;
+
+		if (!isValid(enteredPassword)) {
+			alert(
+				`Invalid password: Password length must be greater than 6 characters`,
+			);
+			return;
+		}
+
+		const url = `https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCYgRd8jQ0QeSi3hwCZHJuWsfI-xex62Z4`;
+
+		const option = {
+			method: 'POST',
+			body: JSON.stringify({
+				idToken,
+				password: enteredPassword,
+				returnSecureToken: false,
+			}),
+		};
+
+		sendRequest({ url, option })
+			.then((response) => {
+				if (response && response.error && response.error.message) {
+					throw new Error(response.error.message);
+				}
+
+				return response;
+			})
+			.then((data) => {
+				authContext.logout();
+				alert(`Successfully update new password, Please re login again`);
+				history.replace('/auth');
+			})
+			.catch((error) => alert(error.message));
+	};
+
+	return (
+		<form onSubmit={submitHandler} className={classes.form}>
+			<div className={classes.control}>
+				<label htmlFor='new-password'>New Password</label>
+				<input
+					minLength='7'
+					ref={passwordInputRef}
+					type='password'
+					id='new-password'
+				/>
+			</div>
+			<div className={classes.action}>
+				<button>Change Password</button>
+			</div>
+		</form>
+	);
+};
 
 export default ProfileForm;
--- src/components/Profile/UserProfile.js
+++ src/components/Profile/UserProfile.js
@@ -1,13 +1,50 @@
+import { useEffect, useContext, useState } from 'react';
 import ProfileForm from './ProfileForm';
 import classes from './UserProfile.module.css';
+import useHttp from '../../hooks/use-http';
+import AuthContext from '../../store/auth-context';
 
 const UserProfile = () => {
-  return (
-    <section className={classes.profile}>
-      <h1>Your User Profile</h1>
-      <ProfileForm />
-    </section>
-  );
+	const fetchUser = useHttp;
+	const authContext = useContext(AuthContext);
+	const userToken = authContext.token;
+	const [userEmail, setUserEmail] = useState(null);
+
+	useEffect(() => {
+		const option = {
+			method: 'POST',
+			body: JSON.stringify({
+				idToken: userToken,
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		};
+		const url = `https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyCYgRd8jQ0QeSi3hwCZHJuWsfI-xex62Z4`;
+
+		if (userToken) {
+			fetchUser({ url, option })
+				.then((response) => {
+					if (response && response.error && response.error.message) {
+						// show error
+						throw new Error(response.error.message);
+					}
+					return response;
+				})
+				.then((userData) => {
+					setUserEmail(userData.users[0].email);
+				})
+				.catch((error) => alert(error.message));
+		}
+	}, [fetchUser, userToken]);
+
+	return (
+		<section className={classes.profile}>
+			<h1>Your User Profile</h1>
+			{userEmail && <h2>{userEmail}</h2>}
+			<ProfileForm />
+		</section>
+	);
 };
 
 export default UserProfile;
