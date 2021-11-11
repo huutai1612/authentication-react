9d03c44b53ade22710cf32d21b3a8d6d0343acc3
huutai1612 <tai.huu1612@gmail.com>
2021-11-11 13:30:54 +0700

    use localstorage to contain our token

--- src/store/AutheContextProvider.js
+++ src/store/AutheContextProvider.js
@@ -2,13 +2,20 @@ import { useState } from 'react';
 import AuthContext from './auth-context';
 
 const AuthContextProvider = (props) => {
-	const [token, setToken] = useState(null);
+	const initialTokenState = localStorage.getItem('token');
+	const [token, setToken] = useState(initialTokenState);
 
 	const isUserLoggedIn = !!token;
 
-	const login = (token) => setToken(token);
+	const login = (token) => {
+		setToken(token);
+		localStorage.setItem('token', token);
+	};
 
-	const logout = () => setToken(null);
+	const logout = () => {
+		setToken(null);
+		localStorage.removeItem('token');
+	};
 
 	const contextValue = {
 		token,
